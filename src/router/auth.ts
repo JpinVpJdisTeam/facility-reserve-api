8b3749a9f5ae17480f1b92113b0ce4c953f58593
Yuki Nishimura <yukinkopen@gmail.com>
2022-02-04 16:15:15 +0900

    不要なコメントを削除

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -39,34 +39,6 @@ export default router(
   post('/api/logout', async () => {
     return { message: 'logout' };
   }),
-  // post('/api/login', async (req, res) => {
-  //   const data = await json(req);
-  //   const { email, password } = data;
-
-  //   // TODO パスワードの暗号化とかセキュアな方法調査(?)
-  //   const { data: users } = await client.from('user-old').select('*').eq('email', email);
-
-  //   if (users.length === 0) {
-  //     send(res, 401, {
-  //       message: 'unauthorized',
-  //     });
-  //     return;
-  //   }
-  //   const user = users[0];
-
-  //   if (user.password !== password) {
-  //     send(res, 401, {
-  //       message: 'unauthorized',
-  //     });
-  //     return;
-  //   }
-
-  //   const token = sign({ id: user.id }, JWT_SECRET, {
-  //     expiresIn: `${process.env.TOKEN_EXPIRED_HOUR}h`,
-  //   });
-
-  //   return { access_token: token };
-  // }),
   post('/api/logout', async () => {
     return { message: 'logout' };
   }),
